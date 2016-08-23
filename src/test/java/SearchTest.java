c0480a0e97a3cb0818f0d31aa023fcef6342172c
Srinath Shil <srinath.shil123@gmail.com>
2016-08-23 09:40:42 +0530

    jj

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,15 +1,11 @@
 
 
 
-	import java.util.regex.Pattern;
 	import java.util.concurrent.TimeUnit;
 	import org.junit.*;
 	import static org.junit.Assert.*;
-	import static org.hamcrest.CoreMatchers.*;
 	import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-	import org.openqa.selenium.support.ui.Select;
+    import org.openqa.selenium.chrome.ChromeDriver;
 
 	public class SearchTest {
 	  private WebDriver driver;
